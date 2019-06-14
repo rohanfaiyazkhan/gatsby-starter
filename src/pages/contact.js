7d7fdf56bbb7b5f444dafb6d78b3a04364331a96
Rohan Faiyaz Khan <rkhan@relisource.com>
2019-06-14 16:50:23 +0600

    First commit

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const ContactPage = () => {
+	return (
+		<Layout>
+			<h1>Contact</h1>
+			<p>
+				The best way to reach me is via{' '}
+				<a href="https://www.twitter.com/RohanFaiyaz" target="_blank">
+					@RohanFaiyaz
+				</a>{' '}
+				on Twitter!
+			</p>
+		</Layout>
+	);
+};
+
+export default ContactPage;
